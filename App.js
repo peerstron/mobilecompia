20cbfe7f9f80eb956c107665e866facaa628b4be
Nyarko Ebenezer <blissful2999@gmail.com>
2021-08-01 22:14:32 -0700

    added expo publish test

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './screens/test';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to IA</Text>
+      <Text>Welcome to IA Test</Text>
+      <Test />
       <StatusBar style="auto" />
     </View>
   );
